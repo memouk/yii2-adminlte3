9487a299f273e318158df4f02fa74330cef58b08
juancho44 <juanclama@gmail.com>
2023-02-21 16:44:50 -0500

    Cambio plantilla

---
+++ src/views/site/index_cliente.php
@@ -0,0 +1,12 @@
+<?php
+
+use dosamigos\chartjs\ChartJs;
+
+$this->title = 'DashBoard';
+$this->params['breadcrumbs'] = [['label' => $this->title]];
+?>
+<div class="container-fluid">
+
+ Vista Cliente 
+
+</div>
