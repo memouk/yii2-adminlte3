210a36d194284ab1adc5133c3a00848ff0da68ed
Jordan Nuñez <ingjordannunez@gmail.com>
2023-05-29 15:03:05 -0500

    subiendo vista cliente

--- src/views/site/index_cliente.php
+++ src/views/site/index_cliente.php
@@ -1,12 +1,17 @@
 <?php
 
 use dosamigos\chartjs\ChartJs;
+use yii\helpers\Html;
 
-$this->title = 'DashBoard';
+$this->title = 'Bienvenidos a Cargranel';
 $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
 <div class="container-fluid">
-
- Vista Cliente 
-
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12" align="center">
+                <?=Html::img(Yii::getAlias('@web').'/imgs/logo-cargranel.png', ['alt' => 'Cargranel', 'class'=>'img-fluid']);?>
+            </div>
+        </div>
+    </div>
 </div>
